259174029c70af5e4831b40571df95583d24c817
VaibhavKumbhar75 <131513816+VaibhavKumbhar75@users.noreply.github.com>
2024-05-15 15:02:35 +0530

    Connection to Database

--- src/main/java/jdbc/employeePayroll/Main.java
+++ src/main/java/jdbc/employeePayroll/Main.java
@@ -9,8 +9,25 @@ public class Main {
 	
 
 	public static void main(String[] args) {
-
+		String dburl = "jdbc:mysql://localhost:3306/payroll_service";
+		String user = "root";
+		String password = "vaibhav";
+		
 		System.out.println("Welcome to EmployeePayroll - JDBC Program");
+
+		try {
+			
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("Driver loaded successfully");
+			
+			Connection connection = DriverManager.getConnection(dburl,user,password);
+			System.out.println("Connection is Successfull...");
+			System.out.println(connection);
+				
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 }
