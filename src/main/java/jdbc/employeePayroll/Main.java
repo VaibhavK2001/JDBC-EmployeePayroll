32c0064407735c2eb874fd24686330a7daccf382
VaibhavKumbhar75 <131513816+VaibhavKumbhar75@users.noreply.github.com>
2024-05-15 14:56:25 +0530

    Welcome Message

---
+++ src/main/java/jdbc/employeePayroll/Main.java
@@ -0,0 +1,16 @@
+package jdbc.employeePayroll;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+	
+	
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to EmployeePayroll - JDBC Program");
+		
+	}
+}
